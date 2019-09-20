be5f949057e37d02fbf7ca80694281ca126172d2
Worome <41673409+Worome@users.noreply.github.com>
2019-09-20 12:56:27 +0200

    Faltan los botones en el diseño activity socios

--- app/src/main/java/com/trianacodes/script/utapp/Socios.java
+++ app/src/main/java/com/trianacodes/script/utapp/Socios.java
@@ -2,8 +2,13 @@ package com.trianacodes.script.utapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Socios extends AppCompatActivity {
+
+    private String CargosArray [] = {"Presidente", "Vicepresidente", "Tesorero", "Secretario", "Vocal"};
+    private Spinner SeleccionCargos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +17,12 @@ public class Socios extends AppCompatActivity {
         setContentView(R.layout.activity_socios);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+
+        SeleccionCargos = findViewById(R.id.sp_cargoSocio);
+
+        ArrayAdapter<String> adaptador_Cargos = new ArrayAdapter<String>(this,
+                R.layout.spinner_item_utapp_cargos ,CargosArray);
+        SeleccionCargos.setAdapter(adaptador_Cargos);
 
     }
 
