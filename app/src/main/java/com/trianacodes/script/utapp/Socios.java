5378cdf87e3a6a8ed78e1983df5640c8b095f7c0
Worome <41673409+Worome@users.noreply.github.com>
2019-09-20 10:20:00 +0200

    Diseño activity inicio terminado

---
+++ app/src/main/java/com/trianacodes/script/utapp/Socios.java
@@ -0,0 +1,13 @@
+package com.trianacodes.script.utapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Socios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_socios);
+    }
+}
